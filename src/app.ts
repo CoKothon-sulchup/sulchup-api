3a18cab150ab7475069281d98b18f7ba8ca4d2d8
Jaewook Ahn <dev.jaewook@gmail.com>
2022-12-29 06:50:19 +0900

    feat(api): Bind API controllers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import dotenv from 'dotenv';
-dotenv.config();
 import express from 'express';
+
+import * as controllers from './controllers';
+
+dotenv.config();
+
 
 const app = express();
 
 app.set('port', process.env.PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/note', controllers.note);
+app.use('/user', controllers.user);
 
 app.get('/ping', (_, res) => {
   res.send('pong');
